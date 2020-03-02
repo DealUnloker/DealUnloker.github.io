ce047c11c698270e6ac2aacf39d673675b1e7e32
DealUnloker <DealUnloker@gmail.com>
2020-03-03 01:54:09 +0200

    bad move

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,25 +1,53 @@
 var slideIndex = 1;
-const slider_lenght = 4;
+const slider_lenght = document.getElementsByClassName('slide').length + 1;
 
+
+//Set first slide z-index: 0
+var current_slide = document.getElementById(`slide_1`);
+current_slide.style.opacity = 1;
 
 function plusSlide() {
-    if (slideIndex == slider_lenght)
-        slideIndex = 1;
-    else
-        slideIndex += 1;
-    showSlides(slideIndex);
+    showSlides(slideIndex + 1)
 }
 
 
 function minusSlide() {
-    if (slideIndex == 1)
-        slideIndex = slider_lenght;
-    else
-        slideIndex -= 1;
-    showSlides(slideIndex);
+    showSlides(slideIndex - 1)
 }
 
-function showSlides(n) {
-    var slide = document.getElementById("slide");
-    slide.style.backgroundImage = `url("../img/slider/slide_${slideIndex}.jpg")`
+function showSlides(next_slide) {
+    var pastIndex = slideIndex;
+    if (next_slide == 0)
+        slideIndex = slider_lenght - 1;
+    else if (next_slide == slider_lenght)
+        slideIndex = 1;
+    else
+        slideIndex = next_slide;
+    var slide = document.getElementById(`slide_${slideIndex}`);
+    var past_slide = document.getElementById(`slide_${pastIndex}`);
+    for (var x = 1; x < slider_lenght; x++) {
+        if (x == pastIndex || x == slideIndex)
+            continue;
+        document.getElementById(`slide_${x}`).style.zIndex = -2;
+    }
+    slide.style.zIndex = 0;
+    past_slide.style.zIndex = -999;
+    fade(slide);
+    past_slide.style.opacity = 0;
+}
+
+
+function fade(element) {
+    var op = 0;
+    var intervalId = setInterval(function() {
+        element.style.opacity = op;
+        op += 0.05;
+        if (op == 1) clearInterval(intervalId);
+    }, 70);
+}
+
+function info() {
+    for (var i = 1; i <= slider_lenght; i++) {
+        alert(document.getElementById(`slide_${i}`).style.zIndex)
+    }
 }
