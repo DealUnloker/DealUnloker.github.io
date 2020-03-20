60b64dca6757213a4c20efd2a6acb6cedb0b47f1
DealUnloker <DealUnloker@gmail.com>
2020-03-20 02:16:00 +0200

    4 of 8 blocks are done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './sass/style.sass'
+import './sass/small.sass'
+import './sass/medium.sass'
+import './sass/large.sass'
+import './sass/extra_large.sass'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-console.log('Oh.. noo..')
