1022506c241ff420dba5a7b49c66dea39f2a2d7c
DealUnloker <DealUnloker@gmail.com>
2020-03-20 20:31:09 +0200

    remake2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/style.sass'
-import './sass/small.sass'
-import './sass/medium.sass'
 import './sass/large.sass'
-import './sass/extra_large.sass'
+import './sass/medium.sass'
+import './sass/small.sass'
+import './sass/extra_small.sass'
 import 'bootstrap/dist/css/bootstrap.min.css';
