8f289949dfa8234d0fcc32eb2c03d1abed0e4859
DealUnloker <DealUnloker@gmail.com>
2020-03-21 19:16:00 +0200

    bootstap is done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import './scripts/slider.js'
 import './sass/style.sass'
 import './sass/large.sass'
 import './sass/medium.sass'
 import './sass/small.sass'
 import './sass/extra_small.sass'
+import './sass/slider.sass'
 import 'bootstrap/dist/css/bootstrap.min.css';
